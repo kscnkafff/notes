548a15da042ff646d98a24909941134cae67475c
kscnkafff <mustafaokanyilmaz10@gmail.com>
2025-08-21 12:12:15 +0300

    Initial Commit

---
+++ notesTests/notesTests.swift
@@ -0,0 +1,17 @@
+//
+//  notesTests.swift
+//  notesTests
+//
+//  Created by Okan Yılmaz on 17.08.2025.
+//
+
+import Testing
+@testable import notes
+
+struct notesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
